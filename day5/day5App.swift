d6d376e9ad1c054b6f0461103d01ed8e8d2eaef2
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-06-09 11:13:25 -0400

    Initial Commit

---
+++ day5/day5App.swift
@@ -0,0 +1,17 @@
+//
+//  day5App.swift
+//  day5
+//
+//  Created by Michelle Elias Flores on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct day5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
